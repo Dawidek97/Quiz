1bf6b071463f8a0e9a86a38d7425d2349205c5b5
Dawidek97 <dawid.rykowski@go2.pl>
2019-03-13 10:38:16 +0100

    added model Question and  test

---
+++ src/main/java/example/quiz/model/Question.java
@@ -0,0 +1,52 @@
+package example.quiz.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String content;
+    private boolean correct;
+
+    @ManyToOne
+    private Test test;
+
+    public Question(Test test) {
+        this.test = test;
+    }
+
+    public Question(String content, boolean correct) {
+        this.content = content;
+        this.correct = correct;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
+    }
+
+    public Test getTest() {
+        return test;
+    }
+
+    public void setTest(Test test) {
+        this.test = test;
+    }
+
+    public Question() {
+    }
+}
---
+++ src/main/java/example/quiz/model/Test.java
@@ -0,0 +1,53 @@
+package example.quiz.model;
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Test {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    // nazwa klucza obcego po drugiej stronie
+    @OneToMany(mappedBy = "test")
+    List<Question> questionList = new ArrayList<>();
+    private String name;
+
+    public Test(List<Question> questionList) {
+        this.questionList = questionList;
+    }
+
+    public Test(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Question> getQuestionList() {
+        return questionList;
+    }
+
+    public void setQuestionList(List<Question> questionList) {
+        this.questionList = questionList;
+    }
+
+    public Test() {
+    }
+}
