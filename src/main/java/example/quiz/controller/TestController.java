f8565019c2d8acdc2447d9fe5194ba340c332121
Dawidek97 <dawid.rykowski@go2.pl>
2019-03-13 14:56:41 +0100

    final

--- src/main/java/example/quiz/controller/TestController.java
+++ src/main/java/example/quiz/controller/TestController.java
@@ -9,21 +9,51 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Scope(value = "session") // dzieki temu kazdy uzytkownik bedzie mial swoj kontroller
+@Scope(value = "session")
 @Controller
 public class TestController {
+
+    private User user = new User();
 
     @Autowired
     private TestRepository testRepository;
 
-    private User user = new User();
+    private Integer currentQuestion;
 
-    @GetMapping("/test/{id}/play")
-    public String play(@PathVariable Integer id, ModelMap modelMap){
+    @GetMapping("/tests/{id}/play")
+    public String play(@PathVariable Integer id, ModelMap modelMap) {
         Test test = testRepository.findById(id).get();
         modelMap.addAttribute("test", test);
-        modelMap.addAttribute("question", test.getQuestionList().get(0));
+        modelMap.addAttribute("question",
+                test.getQuestionList().get(0));
+        currentQuestion = 0;
+        user = new User();
         return "test";
     }
+
+
+    @GetMapping("/tests/{id}/continue")
+    public String continueTest(
+            @RequestParam(required = false)
+                    boolean correct, @PathVariable Integer id, ModelMap modelMap) {
+        Test test = testRepository.findById(id).get();
+        if ( test.getQuestionList().get(currentQuestion).isCorrect() == correct){
+            user.addPoint();
+        }
+        currentQuestion++;
+        if ( currentQuestion < test.getQuestionList().size() ){
+            modelMap.put("test",test);
+            modelMap.put("question",test.getQuestionList().get(currentQuestion));
+        }
+        else{
+            modelMap.put("test",test);
+            modelMap.put("message","Koniec gry! Twoja liczba punktów to: "
+                    +user.getNumberOfPoints());
+        }
+
+        return "test";
+    }
+
 }
--- src/main/java/example/quiz/model/Question.java
+++ src/main/java/example/quiz/model/Question.java
@@ -49,4 +49,6 @@ public class Question {
 
     public Question() {
     }
+
+
 }
