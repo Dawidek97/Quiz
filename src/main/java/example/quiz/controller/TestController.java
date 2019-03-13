28d40202d33d8c36fd8d04900f8abbaa458ea2ce
Dawidek97 <dawid.rykowski@go2.pl>
2019-03-13 12:54:46 +0100

    added html

--- src/main/java/example/quiz/controller/TestController.java
+++ src/main/java/example/quiz/controller/TestController.java
@@ -1,22 +1,29 @@
 package example.quiz.controller;
 
+import example.quiz.model.Test;
 import example.quiz.model.User;
+import example.quiz.repository.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Scope(value = "session") // dzieki temu kazdy uzytkownik bedzie mial swoj kontroller
 @Controller
 public class TestController {
 
-    User user = new User();
-    private int counter;
-    @ResponseBody
+    @Autowired
+    private TestRepository testRepository;
+
+    private User user = new User();
+
     @GetMapping("/test/{id}/play")
-    public String play(@PathVariable Integer id){
-        user.addPoint();
-        return "" + user.getNumberOfPoints();
+    public String play(@PathVariable Integer id, ModelMap modelMap){
+        Test test = testRepository.findById(id).get();
+        modelMap.addAttribute("test", test);
+        modelMap.addAttribute("question", test.getQuestionList().get(0));
+        return "test";
     }
 }
