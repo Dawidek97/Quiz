cf0133a039d573d629c52b3eff86f92c0b3b0f23
Dawidek97 <dawid.rykowski@go2.pl>
2019-03-13 12:22:43 +0100

    added session

--- src/main/java/example/quiz/controller/TestController.java
+++ src/main/java/example/quiz/controller/TestController.java
@@ -1,4 +1,22 @@
 package example.quiz.controller;
 
+import example.quiz.model.User;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Scope(value = "session") // dzieki temu kazdy uzytkownik bedzie mial swoj kontroller
+@Controller
 public class TestController {
+
+    User user = new User();
+    private int counter;
+    @ResponseBody
+    @GetMapping("/test/{id}/play")
+    public String play(@PathVariable Integer id){
+        user.addPoint();
+        return "" + user.getNumberOfPoints();
+    }
 }
